6c72c159a27c056eee1243b80819cb873d61f061
Richard Burnison <richard@burnison.ca>
2012-10-05 15:24:45 -0400

    Added GPL licensing.

--- src/history.c
+++ src/history.c
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2012 Richard Burnison
+ *
+ * This file is part of Clip.
+ *
+ * Clip is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Clip is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Clip.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "history.h"
 
 #include "config.h"
